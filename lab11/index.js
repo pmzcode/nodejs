3e1e2242ba14bad3123e62bf223bfbb7a753665e
Egor Pomazai <pmz-work@yandex.ru>
2017-05-02 14:02:38 +0300

    laba11

---
+++ lab11/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by MSI on 28.04.2017.
+ */
+
+const pmz = require('@egorpmz/lab11git');
+
+
+
+pmz.repositoryByName('accetone')
+//pmz.repositoryByStr('cwp')
+
+var rep={
+    "name": "cwp",
+    "owner":{
+        "login":"accetone"
+
+    }
+}
+
+//pmz.commitsByRepository(rep);
+
+
+
